77bc3ff3dfe75b1678630dc244c6ccce1ff3101d
cordeiro100 <mosscordeiro@hotmail.com>
2022-05-21 12:07:11 -0300

    ctd21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HomeComponent } from './views/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-   
+    HttpClientModule
+    
     
   ],
   providers: [],
